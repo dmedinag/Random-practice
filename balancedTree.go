c15781ec16a0e203c1795ff06ea3de1edf9e2efd
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-12-18 03:59:39 -0500

    Balanced trees in GO

--- balancedTree.go
+++ balancedTree.go
@@ -21,41 +21,37 @@ type TreeNode struct {
 }
 
 /**
- * Return the maximum depth of a given node taken as root
+ * Naive approach: Top down approach, traversing the tree BFS, O(N^2)
  */
-func depth( root *TreeNode ) float64 {
+func depthBFS( root *TreeNode ) float64 {
+    // Fail fast: a tree with no nodes has no length
+    if( root == nil ) { return 0 }
 
-    if( root.Left != nil ) {
-
-        // When both children exist, return the largest depth + 1
-        if( root.Right != nil ) { return math.Max(depth(root.Left), depth(root.Right)) + 1 }
-
-        // When only the left child exists, return left child's + 1
-        return depth( root.Left ) + 1
-    }
-
-    // When only the right child exists, return right child's + 1
-    if( root.Right != nil ) { return depth( root.Right ) + 1 }
-
-    // Return 0 for there's no extra length when there are no children
-    return 0
+    return math.Max( depth(root.Left), depth(root.Right) ) + 1
 }
 
-func isBalanced(root *TreeNode) bool {
+func isBalancedNaive(root *TreeNode) bool {
 
     // Fail fast: an empty tree is balanced
     if( root == nil ) { return true }
-
     // A tree is balanced if both left and right subtrees are balanced and are equally deep with an error of 1 unit
-    if( root.Left != nil ) {
-        if( root.Right != nil ) {
-            return isBalanced( root.Left ) && isBalanced( root.Right ) && math.Abs(depth(root.Left) - depth(root.Right)) <= 1
-        }
-        return depth( root ) <= 1
-    }
-
-    if( root.Right != nil )    { return depth( root ) <= 1 }
-
-    // A tree with only one node is balanced
-    return true
+    return isBalanced( root.Left ) && isBalanced( root.Right ) && math.Abs(depth(root.Left) - depth(root.Right)) <= 1
 }
+
+/**
+ * Smart approach: Bottom up approach, traversing the tree DFS, O(N)
+ */
+func depthDFS( root *TreeNode ) float64 {
+
+    if( root == nil ) { return 0; }
+
+    left := depth( root.Left )
+    if( left == -1 ) { return -1 }
+    right := depth( root.Right )
+    if( right == -1 ) { return -1 }
+
+    if ( math.Abs( left - right ) > 1 ) { return -1 }
+    return math.Max(left, right) + 1
+}
+
+func isBalancedSmart(root *TreeNode) bool { return depth( root ) != -1 }
