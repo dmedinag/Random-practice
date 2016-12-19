913fe272bd8c5a3b2c22dac808c347e77a91b88d
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-12-18 20:21:11 -0500

    Aritmetic subslices

--- containsNearbyAlmostDuplicate.cpp
+++ containsNearbyAlmostDuplicate.cpp
@@ -2,13 +2,18 @@
 // - The difference between nums[i] and nums[j] is at most t.
 // - The difference between i and j is at most k.
 
+#include <vector>
+// #include <cmath>
+
+using namespace std;
+
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         int size = nums.size();
-        for( uint i = 0; i < size; ++i ) {
-            for( uint j = i+1; j-i <= k; ++j ) {
-                if( abs(nums[i] - nums[j]) > t ) continue;
+        for( int i = 0; i < size; ++i ) {
+            for( int j = i+1; j-i <= k; ++j ) {
+                if( fabs(nums[i] - nums[j]) > t ) continue;
                 return true;
             }
         }
